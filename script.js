bca47de3015635891eff93248935b6605575f9d3
wirocent <wirocent@gmail.com>
2025-01-19 16:01:25 +0900

    Initial commit

--- script.js
+++ script.js
@@ -13,6 +13,12 @@ function submitWallet() {
     if (walletInput.value.trim() === '') {
         alert('Please enter a valid wallet address!');
     } else {
+        // 로컬스토리지에 저장
+        const walletAddress = walletInput.value.trim();
+        let walletList = JSON.parse(localStorage.getItem('walletAddresses')) || []; // 기존 데이터 가져오기
+        walletList.push(walletAddress); // 새 주소 추가
+        localStorage.setItem('walletAddresses', JSON.stringify(walletList)); // 저장
+
         // 성공 메시지 표시
         successMessage.style.display = 'block';
 
